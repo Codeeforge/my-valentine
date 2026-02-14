11880a3ab74250f583c5affd7be630d3a03fe425
Codeeforge <codeeforge007@gmail.com>
2026-02-14 21:10:48 +0700

    Initial commit

---
+++ project/script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Kamu yakin?",
+    "beneran??",
+    "Apakah sangat yakin?",
+    "Ayolah sayang...",
+    "Tolong di pikir lagi!",
+    "Kalo kamu bilang tak, Aku akan sangat sedih...",
+    "Aku akan sangat sangat sedih...",
+    "Aku akan benar benar sangat sangat sedih...",
+    "Ok baik, aku akan berhenti bertanya...",
+    "Hanya bercanda, sayang ayolah kumohon! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "ya.html";
+}
